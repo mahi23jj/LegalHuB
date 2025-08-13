cc91060041a05b09b52ba31c652d334e8e70de9c
Dipjyoti Das <126075231+dipexplorer@users.noreply.github.com>
2025-08-13 22:29:09 +0530

    Lawyer (#139)
    * updating lawyer features
    
    * adding apply for lawyer functionality
    
    * improve lawyer specific page
    
    * improve lawyer specific page
    
    * updating error
    
    * adding appointment implementation
    
    * fixing appointment
    
    * adding proper booking system
    
    * fixing bugs
    
    * adding fixes
    
    * adding fixes
    
    * adding fixes
    
    * adding fixes
    
    * adding fixes
    
    * adding updated
    
    * adding chat system
    
    * fixing error and updating chat functionalities
    
    * fixing error and updating chat functionalities
    
    * fixing error

--- src/controllers/adminDashboard.js
+++ src/controllers/adminDashboard.js
@@ -16,7 +16,5 @@ exports.adminApproveLawyer = asyncHandler(async (req, res) => {
     }
     lawyer.isApproved = true;
     await lawyer.save();
-    res.status(200).json(
-        new apiResponse(200, lawyer, "Lawyer approved successfully")
-    );
+    res.status(200).json(new apiResponse(200, lawyer, "Lawyer approved successfully"));
 });
